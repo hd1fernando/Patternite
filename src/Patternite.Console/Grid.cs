ea1a10fe807546c62f8618c1f4793df5a0243b3d
Fernando Gonçalves <hd1.fernandosouza@gmail.com>
2024-02-26 19:21:47 -0300

    feat: add new functionalities into grid class

--- src/Patternite.Console/Grid.cs
+++ src/Patternite.Console/Grid.cs
@@ -9,9 +9,6 @@ public class Grid<T>
         _matrix = new T[rows, cols];
     }
 
-    public static Grid<T> Create<T>(int rows, int cols)
-        => new Grid<T>(rows, cols);
-
     public void Print()
     {
         for (int i = 0; i < _matrix.GetLength(0); i++)
@@ -24,10 +21,32 @@ public class Grid<T>
         }
     }
 
+    public void Pupulate(Func<T> func)
+    {
+        for (int i = 0; i < _matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < _matrix.GetLength(1); j++)
+            {
+                _matrix[i, j] = func();
+            }
+        }
+    }
+
+    public int Rows => _matrix.GetLength(0);
+    public int Cols => _matrix.GetLength(1);
+
     public T this[int row, int col]
     {
         get => _matrix[row, col];
         set => _matrix[row, col] = value;
+    }
+
+    public Grid<T> Clone()
+    {
+        return new Grid<T>(_matrix.GetLength(0), _matrix.GetLength(1))
+        {
+            _matrix = (T[,])_matrix.Clone()
+        };
     }
 
 }
